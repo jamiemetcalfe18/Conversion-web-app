79bf7cd1b9f773df0faa71ddfa76df22d8939ac4
Jamie Metcalfe <jamie.jet18@gmail.com>
2023-03-25 01:38:25 +0000

    Coded out the basic functionality, the math needs to be double checked however. The reset button also works!

--- script.js
+++ script.js
@@ -20,3 +20,39 @@ select.appendChild(option3);
 // Add the select element to an existing div element with ID "myDiv"
 const div = document.getElementById("selectDiv");
 div.appendChild(select);
+
+// calculate function
+function calculate() {
+  const select = document.getElementsByTagName("select")[0];
+  const inputValue = parseFloat(document.getElementById("myInput").value);
+  const selectedOption = select.options[select.selectedIndex].value;
+
+  let result;
+  //le mathematics
+  switch(selectedOption) {
+    case "option1":
+      result = (inputValue - 32) * 5/9;
+      break;
+    case "option2":
+      result = inputValue * 1.60934;
+      break;
+    case "option3":
+      result = inputValue / 128;
+      break;
+    default:
+      result = "Please select an option";
+  }
+  //spits results out
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = "The result is: " + result;
+}
+
+// Get the reset button element
+const resetButton = document.querySelector('input[type="reset"]');
+
+// Add an event listener to the reset button
+resetButton.addEventListener('click', () => {
+  // Clear the input field and result display
+  document.getElementById('myInput').value = '';
+  document.getElementById('result').innerHTML = '';
+});
